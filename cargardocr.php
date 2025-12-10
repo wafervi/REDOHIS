7215f1025e4d590b8690306ec3b674c9349da3ec
Wagner Fernandez <wagnerfernandez17@gmail.com>
2025-12-10 16:30:23 -0500

    files code updated

--- cargardocr.php
+++ cargardocr.php
@@ -114,7 +114,13 @@ while ($row = $sel->fetch_assoc()) {
     </div>
 
     <div class="ml-auto">
-        <a class="btn btn-light" href="logout.php">Cerrar Sesión</a>
+        <a class="btn btn-light d-flex align-items-center" href="logout.php">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2" viewBox="0 0 16 16" aria-hidden="true" focusable="false">
+                <path d="M7.5 1v7h1V1z"/>
+                <path d="M3 8.812a5 5 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812"/>
+            </svg>
+            Cerrar Sesión
+        </a>
     </div>
 </nav>
 
