250b21247f7da0494730f3243524b202575c3c91
Wagner Fernandez <wagnerfernandez17@gmail.com>
2025-12-10 15:44:02 -0500

    button menu deleted

--- cargardocr.php
+++ cargardocr.php
@@ -123,9 +123,7 @@ while ($row = $sel->fetch_assoc()) {
     <div class="p-3">
         <h6>Menú</h6>
         <div class="list-group">
-            <button type="button" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#exampleModal">
-                Recibir Nuevo Documento
-            </button>
+            
             <button type="button" class="list-group-item list-group-item-action d-flex align-items-center" data-toggle="modal" data-target="#exampleModal">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-database-fill mr-2" viewBox="0 0 16 16">
                 <path d="M3.904 1.777C4.978 1.289 6.427 1 8 1s3.022.289 4.096.777C13.125 2.245 14 2.993 14 4s-.875 1.755-1.904 2.223C11.022 6.711 9.573 7 8 7s-3.022-.289-4.096-.777C2.875 5.755 2 5.007 2 4s.875-1.755 1.904-2.223"/>
