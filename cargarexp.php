e295e3612d18eb120d19440c1d974ae3d72f5193
Wagner Fernandez <wagnerfernandez17@gmail.com>
2025-12-17 18:55:53 -0500

    footer updated

--- cargarexp.php
+++ cargarexp.php
@@ -224,7 +224,7 @@ if (empty($_SESSION["usuario"])) {
 
 <!-- etiqueta del pie de página con copyright -->
 <footer class="text-center mt-3 mb-4">
-    <p class="small"> Documentado por: <a href="https://github.com/wafervi" target="_blank">@wafervi</a> - SAGEN / CAGESDO © 2022 - <?php echo date('Y'); ?> -  
+    <p class="small"> Desarrollado por: <a href="https://github.com/wafervi" target="_blank">wafervi</a> - SAGEN / CAGESDO © 2022 - <?php echo date('Y'); ?> -  
         <?php
         // Repositorio GitHub de Wagner Fernández
         $repo = "wafervi/REDOHIS";
